09fb0214040cd17718dd1f9653894fe99c56d461
faker007 <rlaqjadud195@naver.com>
2020-12-18 19:58:01 +0900

    [Client-Done] 리액션 타이머 완성.

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "닌자세유?";
+    } else if (this.score < 400) {
+      this.rank = "살짝 빠르시네요";
+    } else {
+      this.rank = "굼벵이세유?";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
